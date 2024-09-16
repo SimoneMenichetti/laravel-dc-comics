0189078318866eb0da2195ca5c8746f3d59a91e4
Simone Menichetti <simon3menichetti95@gmail.com>
2024-09-16 16:19:33 +0200

    feat:( add root to show details in htm index.blade)

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,4 +4,25 @@
 
 
 @section('content')
+    <div class="container my-5">
+        <div class="row">
+
+        </div>
+    </div>
+
+    <div class="row">
+        @foreach ($comics as $comic)
+            <div class="col-md-4 col-lg-3 mb-4">
+                <div class="card h-100">
+                    <img src="{{ $comic->thumb }}"class="card-img-top" alt="{{ $comic->title }}">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">{{ $comic->title }}</h5>
+                        <p class="card-text">{{ \Illuminate\Support\Str::limit($comic->description, 100) }}</p>
+                        <p class="card-text text-muted">{{ $comic->price }}</p>
+                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary mt-auto">Go to Details</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
