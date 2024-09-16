5d06166b6e9ac866404eeab83bc239cdf12f0d7a
Simone Menichetti <simon3menichetti95@gmail.com>
2024-09-16 16:18:34 +0200

    feat:( add resource controller , add index.blade and root it , add basic template bootstrap and add foreach to html )

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+<h1 class="text-primary">Comics</h1>
+
+
+@section('content')
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Layout base | @yield('titlePage')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     {{-- Includiamo gli assets con la direttiva @vite --}}
     @vite('resources/js/app.js')
 </head>
+
 <body>
 
     @include('partials.navbar')
@@ -21,4 +25,5 @@
     @include('partials.footer')
 
 </body>
+
 </html>
